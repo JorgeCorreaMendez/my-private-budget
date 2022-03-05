86a723fb4cd6b2c66d38315ba2a808178575e7c7
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2022-03-05 12:38:43 +0000

    Feature: added screen add moviments

--- App.js
+++ App.js
@@ -1,22 +1,41 @@
 import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import BudgetHomeScreen from "./screens/BudgetHomeScreen";
-import AddMovementsScreen from "./screens/AddMovementsScreen"
+import AddMovementsScreen from "./screens/AddMovementsScreen";
 
 export default function App() {
   const [screen, setScreen] = useState("home");
+  const [listMovements, setListMovements] = useState([]);
   const numberAccount = parseInt(Math.random() * 9000) + 1000;
 
-  let content = <BudgetHomeScreen numberAccount={numberAccount} changeScreens={setScreen} />
+  const addToListMovements = (moviment) => {
+    setListMovements((currentMovements) => [
+      ...currentMovements,
+      {
+        key: Math.random().toString(),
+        description: moviment.description,
+        matter: moviment.matter,
+        date: moviment.date,
+      },
+    ]);
+  };
 
-  if(screen === "add")
-    content = <AddMovementsScreen/>
-
-  return (
-    <View style={styles.container}>
-      {content}
-    </View>
+  
+  let content = (
+    <BudgetHomeScreen numberAccount={numberAccount} changeScreens={setScreen} />
   );
+
+  if (screen === "add")
+    content = (
+      <AddMovementsScreen
+        numberAccount={numberAccount}
+        balance={-10}
+        changeScreens={setScreen}
+        addMovements={addToListMovements}
+      />
+    );
+
+  return <View style={styles.container}>{content}</View>;
 }
 
 const styles = StyleSheet.create({
--- screens/AddMovementsScreen.js
+++ screens/AddMovementsScreen.js
@@ -1,9 +1,127 @@
-import { StyleSheet } from "react-native";
+import { Alert, Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import InputDate from "../components/TextInput/InputDate";
+import Colors from "../constants/color";
+import Header from "../components/Header";
+import Input from "../components/TextInput/Input";
 
-const AddMovementsScreen = () => {
-  return <></>
+const AddMovementsScreen = ({
+  numberAccount,
+  balance,
+  addMovements,
+  changeScreens,
+}) => {
+  const [description, setDescriptionr] = useState("");
+  const [matter, setMatter] = useState("");
+  const [date, setDate] = useState("");
+
+  const descriptionInputHandler = (inputText) => {
+    setDescriptionr(inputText);
+  };
+
+  const matterInputHandler = (inputText) => {
+    setMatter(inputText.replace(/[^\d.]/g, ""));
+  };
+
+  const dateInputHandler = (inputText) => {
+    setDate(inputText);
+  };
+
+  const newMovement = (typeMatter) => {
+    setMatter(parseFloat(matter));
+
+    if (isNaN(matter) || date === "" || description === "") {
+      Alert.alert("Error", "Debe introducir todos los campos correctamente", [
+        {
+          text: "Volver al Home",
+          style: "default",
+          onPress: () => changeScreens("home"),
+        },
+        { text: "Continuar", style: "default" },
+      ]);
+    } else {
+      if (typeMatter === "remove") setMatter(matter * -1);
+
+      addMovements({ description, matter, date });
+      changeScreens("home");
+    }
+  };
+
+  let colorBalance = "black";
+  if (balance < 0) colorBalance = "red";
+
+  return (
+    <View>
+      <Header title={"Movimientos"} />
+      <View style={styles.container}>
+        <Text style={{ fontSize: 20 }}>Cuenta *{numberAccount}</Text>
+        <Text style={{ fontSize: 20 }}>
+          Saldo: <Text style={{ color: colorBalance }}>{balance}€</Text>
+        </Text>
+      </View>
+
+      <View style={{ justifyContent: "center", alignItems: "center" }}>
+        <Input
+          styleContainer={{ width: "90%" }}
+          iconName="book"
+          placeholder="Descripción del movimiento"
+          value={description}
+          InputHandler={descriptionInputHandler}
+        />
+        <Input
+          styleContainer={{ width: "90%" }}
+          placeholder="Importe"
+          iconName="cash"
+          keyboardType="decimal-pad"
+          value={matter}
+          InputHandler={matterInputHandler}
+        />
+
+        <InputDate
+          styleContainer={{ width: "90%" }}
+          value={date}
+          InputHandler={dateInputHandler}
+        />
+      </View>
+
+      <View style={styles.buttonsAccions}>
+        <TouchableOpacity onPress={() => newMovement("add")}>
+          <Ionicons
+            name="add-circle-sharp"
+            size={100}
+            color={Colors.secundary}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => newMovement("remove")}>
+          <Ionicons
+            name="remove-circle-sharp"
+            size={100}
+            color={Colors.secundary}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    paddingVertical: "10%",
+  },
+
+  buttonsAccions: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "flex-end",
+    paddingTop: "10%",
+  },
+
+  datePickerStyle: {
+    width: "90%",
+  },
+});
 
 export default AddMovementsScreen;
